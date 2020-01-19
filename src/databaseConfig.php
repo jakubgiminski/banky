80da7bb67fe99072eb8cc1ac394460f945087c2d
Jakub Giminski <jakubgiminski@gmail.com>
2020-01-19 15:26:53 +0100

    Reset database config

--- src/databaseConfig.php
+++ src/databaseConfig.php
@@ -1,9 +1,9 @@
 <?php declare(strict_types=1);
 
 return [
-    'host' => '10.19.17.12',
-    'username' => 'root',
-    'password' => 'password',
-    'database' => 'banky',
-    'port' => 3306,
+    'host' => '',
+    'username' => '',
+    'password' => '',
+    'database' => '',
+    'port' => 0000,
 ];
